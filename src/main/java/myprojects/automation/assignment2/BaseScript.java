3791674196c0857b7b3243e0089394f4f57eb318
chumakig86 <chumak_ig@yahoo.com>
2018-05-27 20:25:42 +0300

    BaseScript browser select functionality implemented

--- src/main/java/myprojects/automation/assignment2/BaseScript.java
+++ src/main/java/myprojects/automation/assignment2/BaseScript.java
@@ -2,6 +2,10 @@ package myprojects.automation.assignment2;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import static myprojects.automation.assignment2.utils.Properties.getBrowser;
 
 /**
  * Base script functionality, can be used for all Selenium scripts.
@@ -11,8 +15,18 @@ public abstract class BaseScript {
     /**
      * @return New instance of {@link WebDriver} object.
      */
-    public static WebDriver getChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        return new ChromeDriver();
+    public static WebDriver getDriver() {
+        switch (getBrowser()) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                return new ChromeDriver();
+            case "internet explorer":
+                System.setProperty("webdriver.ie.driver", "src/main/resources/IEDriverServer.exe");
+                return new InternetExplorerDriver();
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+                return new FirefoxDriver();
+            default: throw new UnsupportedOperationException("Method doesn't return WebDriver instance");
+        }
     }
 }
--- src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
+++ src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
@@ -5,17 +5,16 @@ import org.openqa.selenium.WebDriver;
 
 import java.util.concurrent.TimeUnit;
 
-import static myprojects.automation.assignment2.utils.Properties.*;
+import static myprojects.automation.assignment2.utils.Properties.getBaseAdminUrl;
+import static myprojects.automation.assignment2.utils.Properties.getBrowser;
 
 public class LoginTest extends BaseScript {
 
     public static void main(String[] args) throws InterruptedException {
         // TODO Script to execute login and logout steps
 
-         WebDriver driver = getChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriver driver = getDriver();
         driver.get(getBaseAdminUrl());
         System.out.println(getBrowser());
-        // ...
     }
 }
