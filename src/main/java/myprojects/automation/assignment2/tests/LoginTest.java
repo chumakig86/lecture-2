55a381db7aa04b58ce81adcde6337430ca5289dd
chumakig86 <chumak_ig@yahoo.com>
2018-05-28 12:23:11 +0300

    Updated webdrivers. Implemented LoginTest.

--- src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
+++ src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
@@ -1,20 +1,40 @@
 package myprojects.automation.assignment2.tests;
 
 import myprojects.automation.assignment2.BaseScript;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 import static myprojects.automation.assignment2.utils.Properties.getBaseAdminUrl;
-import static myprojects.automation.assignment2.utils.Properties.getBrowser;
 
 public class LoginTest extends BaseScript {
 
     public static void main(String[] args) throws InterruptedException {
-        // TODO Script to execute login and logout steps
-
         WebDriver driver = getDriver();
         driver.get(getBaseAdminUrl());
-        System.out.println(getBrowser());
+
+        WebElement email= driver.findElement(By.id("email"));
+        WebElement passwd = driver.findElement(By.id("passwd"));
+        WebElement submit = driver.findElement(By.name("submitLogin"));
+
+        email.sendKeys("webinar.test@gmail.com");
+        passwd.sendKeys("Xcg7299bnSmMuRLp9ITw");
+        submit.click();
+
+        (new WebDriverWait(driver, 4))
+                .until(ExpectedConditions.presenceOfElementLocated(By.className("img-thumbnail")));
+
+        WebElement user = driver.findElement(By.className("img-thumbnail"));
+        user.click();
+
+        WebElement logout = driver.findElement(By.id("header_logout"));
+        logout.click();
+        driver.quit();
+
     }
 }
